adfe0063ec47c9beb5e53f8b7751c880087994f6
Carlos Leonardo Goes Farias <carlos.leonardo.goes@gmail.com>
2026-07-28 08:41:31 -0300

    Corrige a geração de número aleatório para usar distribuição inteira

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ int main() {
     int tentativas = 1;
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> dis(1, 20);
-    int numeroAleatorio = static_cast<int>(dis(gen)); // Gera um número aleatório entre 1 e 100
+    std::uniform_int_distribution<> dis(1, 20);
+    int numeroAleatorio = dis(gen); // Gera um número aleatório entre 1 e 100
     while (continuar || tentativas < 3) {
         int palpite;
         fmt::print("Tentativa de palpite: {} de 3\n", tentativas);
